c1f915b140388a85ca3e4e4c86e86e39d3351d3f
EgorArefev <117657935+EgorArefev@users.noreply.github.com>
2022-12-19 17:01:30 +0300

    Egor
    Egor

--- player.py
+++ player.py
@@ -10,11 +10,11 @@ WIDTH = 22
 HEIGHT = 33
 COLOR = "#888888"
 JUMP_POWER = 8.5
-GRAVITY = 0.35  # Сила, которая будет тянуть нас вниз
-ANIMATION_DELAY = 0.1  # скорость смены кадров
-ANIMATION_DELAY_2 = 0.1  # скорость смены кадров
-ICON_DIR = os.path.dirname(__file__)  # Полный путь к каталогу с файлами
-ICON_DIR_2 = os.path.dirname(__file__)  # Полный путь к каталогу с файлами
+GRAVITY = 0.35  
+ANIMATION_DELAY = 0.1 
+ANIMATION_DELAY_2 = 0.1 
+ICON_DIR = os.path.dirname(__file__) 
+ICON_DIR_2 = os.path.dirname(__file__)
 
 ANIMATION_RIGHT = [('%s/players/0.png' % ICON_DIR)]
 ANIMATION_LEFT = [('%s/players/l1.png' % ICON_DIR)]
@@ -34,22 +34,22 @@ ANIMATION_STAY_2 = [('%s/players/0_2.png' % ICON_DIR_2, 0.1)]
 class Player(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
-        self.xvel = 0  # скорость перемещения. 0 - стоять на месте
-        self.startX = x  # Начальная позиция Х, пригодится когда будем переигрывать уровень
+        self.xvel = 0  
+        self.startX = x 
         self.startY = y
-        self.yvel = 0  # скорость вертикального перемещения
-        self.onGround = False  # На земле ли я?
+        self.yvel = 0  
+        self.onGround = False  
         self.image = Surface((WIDTH, HEIGHT))
         self.image.fill(Color(COLOR))
-        self.rect = Rect(x, y, WIDTH, HEIGHT)  # прямоугольный объект
-        self.image.set_colorkey(Color(COLOR))  # делаем фон прозрачным
-        #        Анимация движения вправо
+        self.rect = Rect(x, y, WIDTH, HEIGHT) 
+        self.image.set_colorkey(Color(COLOR)) 
+
         boltAnim = []
         for anim in ANIMATION_RIGHT:
             boltAnim.append((anim, ANIMATION_DELAY))
         self.boltAnimRight = pyganim.PygAnimation(boltAnim)
         self.boltAnimRight.play()
-        #        Анимация движения влево
+
         boltAnim = []
         for anim in ANIMATION_LEFT:
             boltAnim.append((anim, ANIMATION_DELAY))
@@ -58,7 +58,7 @@ class Player(sprite.Sprite):
 
         self.boltAnimStay = pyganim.PygAnimation(ANIMATION_STAY)
         self.boltAnimStay.play()
-        self.boltAnimStay.blit(self.image, (0, 0))  # По-умолчанию, стоим
+        self.boltAnimStay.blit(self.image, (0, 0)) 
 
         self.boltAnimJumpLeft = pyganim.PygAnimation(ANIMATION_JUMP_LEFT)
         self.boltAnimJumpLeft.play()
@@ -72,28 +72,28 @@ class Player(sprite.Sprite):
     def update(self, left, right, up, platforms):
 
         if up:
-            if self.onGround:  # прыгаем, только когда можем оттолкнуться от земли
+            if self.onGround: 
                 self.yvel = -JUMP_POWER
             self.image.fill(Color(COLOR))
             self.boltAnimJump.blit(self.image, (0, 0))
 
         if left:
-            self.xvel = -MOVE_SPEED  # Лево = x- n
+            self.xvel = -MOVE_SPEED
             self.image.fill(Color(COLOR))
-            if up:  # для прыжка влево есть отдельная анимация
+            if up:
                 self.boltAnimJumpLeft.blit(self.image, (0, 0))
             else:
                 self.boltAnimLeft.blit(self.image, (0, 0))
 
         if right:
-            self.xvel = MOVE_SPEED  # Право = x + n
+            self.xvel = MOVE_SPEED
             self.image.fill(Color(COLOR))
             if up:
                 self.boltAnimJumpRight.blit(self.image, (0, 0))
             else:
                 self.boltAnimRight.blit(self.image, (0, 0))
 
-        if not (left or right):  # стоим, когда нет указаний идти
+        if not (left or right):
             self.xvel = 0
             if not up:
                 self.image.fill(Color(COLOR))
@@ -102,53 +102,51 @@ class Player(sprite.Sprite):
         if not self.onGround:
             self.yvel += GRAVITY
 
-        self.onGround = False  # Мы не знаем, когда мы на земле((
+        self.onGround = False
         self.rect.y += self.yvel
         self.collide(0, self.yvel, platforms)
 
-        self.rect.x += self.xvel  # переносим свои положение на xvel
+        self.rect.x += self.xvel
         self.collide(self.xvel, 0, platforms)
 
     def collide(self, xvel, yvel, platforms):
         for p in platforms:
-            if sprite.collide_rect(self, p):  # если есть пересечение платформы с игроком
+            if sprite.collide_rect(self, p):
 
-                if xvel > 0:  # если движется вправо
-                    self.rect.right = p.rect.left  # то не движется вправо
+                if xvel > 0:
+                    self.rect.right = p.rect.left
 
-                if xvel < 0:  # если движется влево
-                    self.rect.left = p.rect.right  # то не движется влево
+                if xvel < 0:
+                    self.rect.left = p.rect.right
 
-                if yvel > 0:  # если падает вниз
-                    self.rect.bottom = p.rect.top  # то не падает вниз
-                    self.onGround = True  # и становится на что-то твердое
-                    self.yvel = 0  # и энергия падения пропадает
+                if yvel > 0:
+                    self.rect.bottom = p.rect.top
+                    self.onGround = True
+                    self.yvel = 0
 
-                if yvel < 0:  # если движется вверх
-                    self.rect.top = p.rect.bottom  # то не движется вверх
-                    self.yvel = 0  # и энергия прыжка пропадает
+                if yvel < 0:
+                    self.rect.top = p.rect.bottom
+                    self.yvel = 0
 
 
 
 class Player_2(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
-        self.xvel = 0  # скорость перемещения. 0 - стоять на месте
-        self.startX = x  # Начальная позиция Х, пригодится когда будем переигрывать уровень
+        self.xvel = 0
+        self.startX = x
         self.startY = y
-        self.yvel = 0  # скорость вертикального перемещения
-        self.onGround = False  # На земле ли я?
+        self.yvel = 0
+        self.onGround = False
         self.image = Surface((WIDTH, HEIGHT))
         self.image.fill(Color(COLOR))
-        self.rect = Rect(x, y, WIDTH, HEIGHT)  # прямоугольный объект
-        self.image.set_colorkey(Color(COLOR))  # делаем фон прозрачным
-        #        Анимация движения вправо
+        self.rect = Rect(x, y, WIDTH, HEIGHT)
+        self.image.set_colorkey(Color(COLOR))
         boltAnim = []
         for anim in ANIMATION_RIGHT_2:
             boltAnim.append((anim, ANIMATION_DELAY_2))
         self.boltAnimRight = pyganim.PygAnimation(boltAnim)
         self.boltAnimRight.play()
-        #        Анимация движения влево
         boltAnim = []
         for anim in ANIMATION_LEFT_2:
             boltAnim.append((anim, ANIMATION_DELAY_2))
@@ -157,7 +155,7 @@ class Player_2(sprite.Sprite):
 
         self.boltAnimStay = pyganim.PygAnimation(ANIMATION_STAY_2)
         self.boltAnimStay.play()
-        self.boltAnimStay.blit(self.image, (0, 0))  # По-умолчанию, стоим
+        self.boltAnimStay.blit(self.image, (0, 0))
 
         self.boltAnimJumpLeft = pyganim.PygAnimation(ANIMATION_JUMP_LEFT_2)
         self.boltAnimJumpLeft.play()
@@ -171,28 +169,28 @@ class Player_2(sprite.Sprite):
     def update(self, a, d, w, platforms):
 
         if w:
-            if self.onGround:  # прыгаем, только когда можем оттолкнуться от земли
+            if self.onGround:
                 self.yvel = -JUMP_POWER
             self.image.fill(Color(COLOR))
             self.boltAnimJump.blit(self.image, (0, 0))
 
         if a:
-            self.xvel = -MOVE_SPEED  # Лево = x- n
+            self.xvel = -MOVE_SPEED
             self.image.fill(Color(COLOR))
-            if w:  # для прыжка влево есть отдельная анимация
+            if w:
                 self.boltAnimJumpLeft.blit(self.image, (0, 0))
             else:
                 self.boltAnimLeft.blit(self.image, (0, 0))
 
         if d:
-            self.xvel = MOVE_SPEED  # Право = x + n
+            self.xvel = MOVE_SPEED
             self.image.fill(Color(COLOR))
             if w:
                 self.boltAnimJumpRight.blit(self.image, (0, 0))
             else:
                 self.boltAnimRight.blit(self.image, (0, 0))
 
-        if not (a or d):  # стоим, когда нет указаний идти
+        if not (a or d):
             self.xvel = 0
             if not w:
                 self.image.fill(Color(COLOR))
@@ -201,28 +199,28 @@ class Player_2(sprite.Sprite):
         if not self.onGround:
             self.yvel += GRAVITY
 
-        self.onGround = False  # Мы не знаем, когда мы на земле((
+        self.onGround = False
         self.rect.y += self.yvel
         self.collide(0, self.yvel, platforms)
 
-        self.rect.x += self.xvel  # переносим свои положение на xvel
+        self.rect.x += self.xvel
         self.collide(self.xvel, 0, platforms)
 
     def collide(self, xvel, yvel, platforms):
         for p in platforms:
-            if sprite.collide_rect(self, p):  # если есть пересечение платформы с игроком
+            if sprite.collide_rect(self, p):
 
-                if xvel > 0:  # если движется вправо
-                    self.rect.d = p.rect.a  # то не движется вправо
+                if xvel > 0:
+                    self.rect.d = p.rect.a
 
-                if xvel < 0:  # если движется влево
-                    self.rect.a = p.rect.d  # то не движется влево
+                if xvel < 0:
+                    self.rect.a = p.rect.d
 
-                if yvel > 0:  # если падает вниз
-                    self.rect.s = p.rect.s  # то не падает вниз
-                    self.onGround = True  # и становится на что-то твердое
-                    self.yvel = 0  # и энергия падения пропадает
+                if yvel > 0:
+                    self.rect.s = p.rect.s
+                    self.onGround = True
+                    self.yvel = 0
 
-                if yvel < 0:  # если движется вверх
-                    self.rect.w = p.rect.w  # то не движется вверх
-                    self.yvel = 0  # и энергия прыжка пропадает
+                if yvel < 0:
+                    self.rect.w = p.rect.w
+                    self.yvel = 0
