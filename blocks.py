6d57673c355a44408a8f8d046d3ac2f6c726b2b9
Mattew-Kentor <popok3580@gmail.com>
2022-12-19 17:25:42 +0300

    Matvey

--- blocks.py
+++ blocks.py
@@ -1,4 +1,4 @@
-from mains import DISPLAY
+from main import DISPLAY
 
 
 from pygame import *
@@ -6,14 +6,14 @@ import os
 
 PLATFORM_WIDTH = 26
 PLATFORM_HEIGHT = 26
-ICON_DIR = os.path.dirname(__file__)
+ICON_DIR = os.path.dirname(__file__) #  Полный путь к каталогу с файлами
 
 
 class Platform(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
         self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-        self.image = image.load("%s/blocks/platform.png" % ICON_DIR)
+        self.image = image.load("%s/blocks/platform1.png" % ICON_DIR)
         self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
 
 
